6d7f5a15303833583efb85bfc495dfd596d9b3b5
zqiaohe <zanaevab@gmail.com>
2025-01-28 01:50:12 +0300

    hw 01

---
+++ 01/docker_bash_script.sh
@@ -0,0 +1,2 @@
+docker run -i --entrypoint=bash python:3.12.8
+docker compose up
---
+++ 01/postgres_bash_script.sh
@@ -0,0 +1,1 @@
+wget https://github.com/DataTalksClub/nyc-tlc-data/releases/download/green/green_tripdata_2019-10.csv.gz
